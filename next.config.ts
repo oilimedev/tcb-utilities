a3861d65e5dadc7d97a0b543bd506638246bba90
Emilio Gomez <emiliogmztj@gmail.com>
2026-08-20 20:42:36 -0700

    Updated config files.

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  //output: "standalone",  for cloudflare deploy
   output: "export",
   basePath: "/tcb-utilities",
   images: {
